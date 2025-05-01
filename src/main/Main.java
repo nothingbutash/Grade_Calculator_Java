bdc3d6643758f5d7034814621653223af7e8bd6e
adaroz01 <adaroz01@students.cs.ubc.ca>
2025-05-01 16:00:25 -0700

    added some stuff

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,32 @@
 ﻿public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {                
+        doPOLI110();    
+    }
+
+    //88.6923076923
+    public static void doPOLI110() {
+        private MarkedThingy midTerm = new MarkedThingy(0);
+        private Mark midTermMark = new Mark();
+
+        private MarkedThingy hW = new MarkedThingy(0);
+        private MarkedThingy hW1 = new MarkedThingy(0);
+        private Mark hW1Mark = new Mark();
+        private MarkedThingy hW2 = new MarkedThingy(0);
+        private Mark hW2Mark = new Mark();
+        private MarkedThingy hW3 = new MarkedThingy(0);
+        private Mark hW3Mark = new Mark();
+        private MarkedThingy hW4 = new MarkedThingy(0);
+        private Mark hW4Mark = new Mark();
+        private MarkedThingy hW5 = new MarkedThingy(0);
+        private Mark hW5Mark = new Mark();
+
+        private markedThingy attendance = new MarkedThingy(0);
+        private Mark attendanceMark = new Mark();
+
+        midTerm.setWeight(25);
+        midTermMark.setMark(20.9/25 * 100);
+        midTerm.addSubGradable(midTermMark);
+
+        hW.addSubGradable(hW1);
     }
 }
