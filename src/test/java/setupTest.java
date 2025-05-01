d2eecd59e69da6828a6845bf6429de2348256804
Trevor Liu <trevor.liu@gmail.com>
2025-05-01 16:16:00 -0700

    added test suite

---
+++ src/test/java/setupTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class setupTest {
+    @Test
+    void test1() {
+        assertEquals(2, 1+1);
+    }
+}
