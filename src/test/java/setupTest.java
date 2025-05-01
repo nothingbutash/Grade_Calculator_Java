cc04ecb771a08c6523b40b61ae8c06823aa4ac55
adaroz01 <adaroz01@students.cs.ubc.ca>
2025-05-01 16:32:24 -0700

    added some stuff for test case

--- src/test/java/setupTest.java
+++ src/test/java/setupTest.java
@@ -6,4 +6,26 @@ public class setupTest {
     void test1() {
         assertEquals(2, 1+1);
     }
+
+    @Test
+    void test2() {
+        double expectedAvg  = 88.6923076923;
+        MarkedThingy midTerm = new MarkedThingy(0);
+        Mark midTermMark = new Mark(20.9/25 * 100);
+
+        MarkedThingy hW = new MarkedThingy(0);
+        MarkedThingy hW1 = new MarkedThingy(0);
+        Mark hW1Mark = new Mark(4/5);
+        MarkedThingy hW2 = new MarkedThingy(0);
+        Mark hW2Mark = new Mark(3.15/5);
+        MarkedThingy hW3 = new MarkedThingy(0);
+        Mark hW3Mark = new Mark(5/5);
+        MarkedThingy hW4 = new MarkedThingy(0);
+        Mark hW4Mark = new Mark(4.6/5);
+        MarkedThingy hW5 = new MarkedThingy(0);
+        Mark hW5Mark = new Mark(5/5);
+
+        MarkedThingy attendance = new MarkedThingy(0);
+        Mark attendanceMark = new Mark(100);
+    }
 }
