477817fedae6042b9defd6b7841d6f2bf6ff72b9
adaroz01 <adaroz01@students.cs.ubc.ca>
2025-05-01 14:20:09 -0700

    added the getMark implementations, as well as the composite add/remove behaviout

--- src/main/MarkedThingy.java
+++ src/main/MarkedThingy.java
@@ -3,21 +3,41 @@ import java.util.ArrayList;
 
 public class MarkedThingy implements Gradable {
     private Collection<Gradable> subGradables;
+    private double weight;
 
     public MarkedThingy() {
-        
+        weight = -1;
     }
     
     public double getMark() {
-        return 1.0;//stub
+        Collection<Gradable> subGradables = getSubGradables();
+        double weightsTimesMarksSum = 0;
+        double weights = 0;
+        for (Gradable subGradable : subGradables) {
+            if (subGradable.getMark() >= 0) {
+                if (subGradable.getClass() == new MarkedThingy().getClass()) {
+                    MarkedThingy subMarkedThingy = (MarkedThingy) subGradable;
+                    weightsTimesMarksSum += (subMarkedThingy.getMark() * subMarkedThingy.getWeight());
+                    weights += subMarkedThingy.getWeight();
+                } else if (subGradable.getClass() == new MarkedThingy().getClass()) {
+                    weightsTimesMarksSum += subGradable.getMark();
+                    weights++;
+                }
+            }            
+        }
+        return weightsTimesMarksSum/weights;
     }
 
-    public void addSubGradable() {
-
+    public double getWeight() {
+        return weight;
     }
 
-    public void removeSubGradable() {
+    public void addSubGradable(Gradable subGradable) {
+        subGradables.add(subGradable);
+    }
 
+    public void removeSubGradable(Gradable subGradable) {
+        subGradables.remove(subGradable);
     }
 
     public Collection<Gradable> getSubGradables() {
